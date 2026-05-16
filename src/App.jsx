aa44d1cf048a7aaaed28cfd00db78f15218e17f5
Abhishek <abhiitc22@gmail.com>
2026-05-16 07:34:31 +0100

    change subheading

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ function App() {
   return (
     <>
 
-   <h1> Learn react with Abhishek Super Brain</h1>
- <p>This is a simple React app.</p>
+    <h1> Learn react with Abhishek Super Brain</h1>
+    <p>This is a simple React app.</p>
+      <p>i am adding an extra line of code</p>
+
    </>
   );
 }
